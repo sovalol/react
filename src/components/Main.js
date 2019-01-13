f6907baeb4ce4d2b1fdbfe0a95480133debc152f
= <=>
2019-01-13 20:00:36 +0300

    Третий урок

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Slider from './Slider'
+
 
 const Repair = styled.div`
 	color: #fff;
@@ -58,7 +60,7 @@ class Main extends React.Component {
 					</MainBtn>
 				</Col>
 				<Col lg={6} lgOffset={1}>
-					
+					<Slider />
 				</Col>
 			</Row>
 		)
